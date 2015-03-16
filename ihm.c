42f30b6651ae6e871b2cc54bb62b30d300dc57be
Laurent Treguier <laurent.treguier@isen.fr>
2015-03-16 09:23:21 +0100

    :)

--- ihm.c
+++ ihm.c
@@ -1,44 +1,54 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+#include <string.h>
 #include "ihm.h"
 
 void ihm(int* e1, int* e2)
 {
-	SDL_Window* window = NULL;
-	SDL_Renderer* renderer = NULL;
-	TTF_Font* font = NULL;
-	SDL_Surface* input = NULL;
-	SDL_Texture* inputTex = NULL;
-	// SDL_Rect
-	SDL_Color white = {
-		255,
-		255,
-		255,
-		255
-	};
+	static SDL_Window* window = NULL;
+	static SDL_Renderer* renderer = NULL;
+	text_t text;
+	
+	text.font = NULL;
+	text.content = NULL;
+	text.texture = NULL;
+	text.rect = (SDL_Rect){10, 10, 0, 64};
+	text.color = (SDL_Color){255, 255, 255, 255};
 	
 	SDL_Init(SDL_INIT_VIDEO);
 	TTF_Init();
 	
 	window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-	font = TTF_OpenFont("./SourceCodePro-Regular.otf", 16);
-	input = TTF_RenderText_Solid(font, "LOL", white);
-	inputTex = SDL_CreateTextureFromSurface(renderer, input);
+	
+	text.font = TTF_OpenFont("./SourceCodePro-Regular.otf", 64);
+	changeText(renderer, &text, "Nombre: 1654");
 	
 	SDL_SetRenderDrawColor(renderer, 31, 31, 31, 255);
 	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, inputTex, NULL, NULL);
+	SDL_RenderCopy(renderer, text.texture, NULL, &(text.rect));
 	SDL_RenderPresent(renderer);
 	
 	SDL_Delay(1500);
 	
-	SDL_DestroyTexture(inputTex);
-	SDL_FreeSurface(input);
-	TTF_CloseFont(font);
+	SDL_DestroyTexture(text.texture);
+	SDL_FreeSurface(text.content);
+	TTF_CloseFont(text.font);
+	
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	
 	TTF_Quit();
 	SDL_Quit();
+}
+
+void changeText(SDL_Renderer* renderer, text_t* text, char* content)
+{
+	if(text->content)
+		SDL_FreeSurface(text->content);
+	
+	if(text->texture)
+		SDL_DestroyTexture(text->texture);
+	
+	text->content = TTF_RenderText_Blended(text->font, content, text->color);
+	text->texture = SDL_CreateTextureFromSurface(renderer, text->content);
+	text->rect.w = text->rect.h / 2 * strlen(content);
 }
--- ihm.h
+++ ihm.h
@@ -1,3 +1,17 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
+typedef struct text_t
+{
+	TTF_Font* font;
+	SDL_Surface* content;
+	SDL_Texture* texture;
+	SDL_Rect rect;
+	SDL_Color color;
+} text_t;
+
 void ihm(int* e1, int* e2);
+
+void changeText(SDL_Renderer* renderer, text_t* text, char* content);
