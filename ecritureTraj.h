1a09f43a1aafb11f0f14530e7438b524bd5dfff9
Laurent Treguier <laurent.treguier@isen.fr>
2015-03-26 10:52:42 +0100

    pre-apocalypse

--- ecritureTraj.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void ecritureTraj(float* tt1, float* tt2, float* tt3, int* ttr, int np);
--- input.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-int inputCheck(int* tabE1E2);
-int add(int* tabE1E2);
-char* conversion(int somme);
